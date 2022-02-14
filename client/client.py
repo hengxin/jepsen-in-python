1257736ad7452c77bc9dc4ee6ef0876b82f99e4e
jiangnanweishao999 <2764065464@qq.com>
2022-02-14 17:05:17 +0800

    add: 远程搭建数据库 简单编写 etcd例

--- client/client.py
+++ client/client.py
@@ -4,8 +4,8 @@
 # @Email   : 2764065464@qq.com
 # @File    : clients.py
 import paramiko
-import db.database as db_operation
-
+from db.database import database_op
+from threading import Thread
 
 class client:
     def __init__(self, hostname, port, username, passwd):
@@ -14,13 +14,41 @@ class client:
         self.username = username
         self.passwd = passwd
         self.ssh_connection = paramiko.SSHClient()
+        self.connect_ssh()
         # todo 下面是将database中的对于数据库的操作函数持有化 暂时为占位
-        self.database_connect = db_operation
-        self.database_drop = db_operation
-        self.database_operate = db_operation
+        self.database = database_op(self.ssh_connection)
 
     def connect_ssh(self):
+        self.ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh_connection.connect(hostname=self.hostname,
                                     port=self.port,
                                     username=self.username,
                                     password=self.passwd)
+
+    def setup_db(self):
+        return self.database.setup
+
+    def shutdown_db(self):
+        self.database.shutdown()
+
+    def connect_db(self):
+        self.database.connect_database()
+
+    def operation(self, string: type, *args):
+        if type == "w":
+            self.database.write(*args)
+        elif type == "r":
+            self.database.read()
+        elif type == "cas":
+            self.database.cas(*args)
+
+
+
+
+if __name__ == '__main__':
+    # client = client(hostname="42.192.52.249", port=22, username="root", passwd="passwd")
+    t1 = Thread(target=client.setup_db())
+    t1.start()
+    t1.join(10)
+    print("a")
+    print("stop")
--- db/database.py
+++ db/database.py
@@ -3,3 +3,38 @@
 # @Author  : jiangnanweishao999
 # @Email   : 2764065464@qq.com
 # @File    : database.py
+import eventlet
+
+
+class database_op:
+    def __init__(self, ssh_connection):
+        self.ssh_connection = ssh_connection
+
+    def setup(self):
+        command_list = [
+            'wget https://storage.googleapis.com/etcd/v3.1.5/etcd-v3.1.5-linux-amd64.tar.gz',
+            'tar -zxvf etcd-v3.1.5-linux-amd64.tar.gz',
+            'mv etcd-v3.1.5-linux-amd64 etcd',
+            '/root/etcd/etcd --log-output stdout --name 42.192.52.249 --listen-peer-urls http://0.0.0.0:2380 --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://42.192.52.249:2379 --initial-cluster-state new --initial-advertise-peer-urls http://42.192.52.249:2380 --initial-cluster 8.133.161.48=http://8.133.161.48:2380,42.192.52.249=http://42.192.52.249:2380 >/root/etcd/etcd.log &'
+
+        ]
+        for c in command_list:
+            stdin, stdout, stderr = self.ssh_connection.exec_command(c)
+            print(stdout.readlines())
+        self.ssh_connection.close()
+        pass
+
+    def shutdown(self):
+        pass
+
+    def connect_database(self):
+        pass
+
+    def write(self, s):
+        pass
+
+    def read(self):
+        pass
+
+    def cas(self, a, b):
+        pass
