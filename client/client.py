f266ee3bb6cf1f56a31ea118a37651f422962c3d
jiangnanweishao999 <2764065464@qq.com>
2022-02-15 14:38:25 +0800

    add: 数据库启动链接操作简单填入

--- client/client.py
+++ client/client.py
@@ -6,9 +6,10 @@
 import paramiko
 from db.database import database_op
 from threading import Thread
+import time
 
 class client:
-    def __init__(self, hostname, port, username, passwd):
+    def __init__(self, hostname, port, username, passwd, *args):
         self.hostname = hostname
         self.port = port
         self.username = username
@@ -16,7 +17,12 @@ class client:
         self.ssh_connection = paramiko.SSHClient()
         self.connect_ssh()
         # todo 下面是将database中的对于数据库的操作函数持有化 暂时为占位
-        self.database = database_op(self.ssh_connection)
+        self.database = database_op(self.ssh_connection, self.hostname, 2379, *args)
+        t1 = Thread(target=self.setup_db())
+        t1.start()
+        t1.join(20)
+        print("database setup finished")
+        self.connect_db()
 
     def connect_ssh(self):
         self.ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -34,11 +40,11 @@ class client:
     def connect_db(self):
         self.database.connect_database()
 
-    def operation(self, string: type, *args):
+    def operation(self, type, *args):
         if type == "w":
             self.database.write(*args)
         elif type == "r":
-            self.database.read()
+            self.database.read(*args)
         elif type == "cas":
             self.database.cas(*args)
 
@@ -46,9 +52,12 @@ class client:
 
 
 if __name__ == '__main__':
-    # client = client(hostname="42.192.52.249", port=22, username="root", passwd="passwd")
-    t1 = Thread(target=client.setup_db())
-    t1.start()
-    t1.join(10)
-    print("a")
-    print("stop")
+    initial_cluster = "42.192.52.249=http://42.192.52.249:2380"
+    client = client("42.192.52.249",22,"root", "passwd", initial_cluster)
+    time.sleep(4)
+    client.operation("w", 'foo', 'hello')
+    client.operation('r', 'foo')
+    client.operation("w", 'foo', 'world')
+    client.operation('r', 'foo')
+    client.shutdown_db()
+
--- db/database.py
+++ db/database.py
@@ -3,20 +3,33 @@
 # @Author  : jiangnanweishao999
 # @Email   : 2764065464@qq.com
 # @File    : database.py
-import eventlet
+import etcd3
 
 
 class database_op:
-    def __init__(self, ssh_connection):
+    def __init__(self, ssh_connection, hostname, port, *args):
         self.ssh_connection = ssh_connection
+        self.hostname = hostname
+        self.port = port
+        self.initial_cluster = args[0]
+        self.database_connection = None
 
     def setup(self):
         command_list = [
             'wget https://storage.googleapis.com/etcd/v3.1.5/etcd-v3.1.5-linux-amd64.tar.gz',
             'tar -zxvf etcd-v3.1.5-linux-amd64.tar.gz',
             'mv etcd-v3.1.5-linux-amd64 etcd',
-            '/root/etcd/etcd --log-output stdout --name 42.192.52.249 --listen-peer-urls http://0.0.0.0:2380 --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://42.192.52.249:2379 --initial-cluster-state new --initial-advertise-peer-urls http://42.192.52.249:2380  >/root/etcd/etcd.log &'
+            '''/root/etcd/etcd --log-output stdout --name {0} --listen-peer-urls http://0.0.0.0:2380 --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://{0}:2379 --initial-cluster-state new --initial-advertise-peer-urls http://{0}:2380 --initial-cluster {1}'''.format(self.hostname, self.initial_cluster)
+        ]
+        for c in command_list:
+            stdin, stdout, stderr = self.ssh_connection.exec_command(c)
+            print(stdout.readlines())
+        pass
 
+    def shutdown(self):
+        command_list = [
+            '''ps -ef|grep etcd|grep -v grep|awk '{print $2}'|xargs kill -9''',
+            'rm -rf /root/*'
         ]
         for c in command_list:
             stdin, stdout, stderr = self.ssh_connection.exec_command(c)
@@ -24,17 +37,18 @@ class database_op:
         self.ssh_connection.close()
         pass
 
-    def shutdown(self):
-        pass
-
     def connect_database(self):
+        self.database_connection = etcd3.client(host=self.hostname, port=self.port)
+
+    def write(self, key, value):
+        self.database_connection.put(key, value)
         pass
 
-    def write(self, s):
+    def read(self, key):
+        result = self.database_connection.get(key)
+        print(result)
         pass
 
-    def read(self):
-        pass
-
-    def cas(self, a, b):
+    # compare and set
+    def cas(self, key, value_old, value_new):
         pass
