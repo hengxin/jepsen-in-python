bb4125ba512540b54bee8f72a14cb7ca577391d6
jiangnanweishao999 <2764065464@qq.com>
2022-04-21 22:56:10 +0800

    add: 增加文档示例

--- pyjepsen.py
+++ pyjepsen.py
@@ -18,7 +18,8 @@ import time
 import random
 from fastcore.transform import Pipeline
 from functools import partial
-from generator import generator as gen, interpreter as gen_inter
+from generator import interpreter as gen_inter
+from generator import generator as gen
 from util.globalvars import GlobalVars
 
 
@@ -94,121 +95,121 @@ def test(generator=None, database=database_op, operation=None):
 
 
 if __name__ == '__main__':
-    def operation(database_connection, history):
-        function_name = history["f"]
-        try:
-            if function_name == "write":
-                database_connection.put("foo", history["value"])
-                return {
-                    "type": "ok",
-                    "f": "write",
-                    "value": history["value"]
-                }
-            elif function_name == "read":
-                read_result = database_connection.get("foo")
-                return {
-                    "type": "ok",
-                    "f": "read",
-                    "value": int(read_result[0]) if read_result[0] else None
-                }
-            elif function_name == "cas":
-                cas_result = database_connection.replace("foo", history["value"][0], history["value"][1])
-                return {
-                    "type": "ok" if cas_result else "fail",
-                    "f": "cas",
-                    "value": history["value"]
-                }
-        except Exception as e:
-            logging.error(traceback.format_exc())
-            logging.error(repr(e))
-            return {
-                "type": "info",
-                "f": function_name,
-                "value": None
-            }
-
-
-    def write():
-        return {
-            "type": "invoke",
-            "f": "write",
-            "value": str(random.randint(1, 5))
-        }
-
-
-    def read():
-        return {
-            "type": "invoke",
-            "f": "read",
-            "value": None
-        }
-
-
-    def cas():
-        return {
-            "type": "invoke",
-            "f": "cas",
-            "value": [str(random.randint(1, 5)), str(random.randint(1, 5))]
-        }
-
-
-    class etcd_database(database_op):
-        def __init__(self, ssh_client, hostname, config):
-            super().__init__(ssh_client, hostname, config)
-            self.port = config["port"]
-            self.initial_cluster = config["initial_cluster"]
-            self.database_connection = None
-
-        def setup(self):
-            root_path = self.ssh_client.pwd() + "/tmp/"
-            self.ssh_client.wget(url="https://storage.googleapis.com/etcd/v3.1.5/etcd-v3.1.5-linux-amd64.tar.gz")
-            self.ssh_client.mkdir(root=root_path, filename="etcd")
-            self.ssh_client.unzip(file_name="{}etcd-v3.1.5-linux-amd64.tar.gz".format(root_path),
-                                  opts={
-                                      "-C": root_path + "/etcd",
-                                      "--strip-components": 1
-                                  })
-            self.ssh_client.touch(root=root_path, filename="etcd.log")
-            self.ssh_client.exec_sudo_command(command="{}etcd/etcd".format(root_path),
-                                              opts={
-                                                  "--log-output": "stdout",
-                                                  "--name": self.hostname,
-                                                  "--listen-peer-urls": "http://0.0.0.0:2380",
-                                                  "--listen-client-urls": "http://0.0.0.0:2379",
-                                                  "--advertise-client-urls": "http://{0}:2379".format(self.hostname),
-                                                  "--initial-cluster-state": "new",
-                                                  "--initial-advertise-peer-urls": "http://{0}:2380".format(
-                                                      self.hostname),
-                                                  "--initial-cluster": self.initial_cluster,
-                                                  "1>{}etcd.log".format(root_path): ""
-                                              })
-
-        def shutdown(self):
-            root_path = self.ssh_client.pwd() + "/*"
-            self.ssh_client.kill_by_process("etcd")
-            self.ssh_client.exec_sudo_command("rm -rf {}".format(root_path))
-
-        def connect_database(self):
-            self.database_connection = etcd3.client(host=self.hostname, port=self.port)
-
-        def disconnect_database(self):
-            if self.database_connection:
-                self.database_connection.close()
-                self.database_connection = None
-            else:
-                pass
-
-        def is_running(self):
-            running = self.ssh_client.exec_command("ps -ef|grep etcd|grep -v grep|grep -v wget|awk '{print $2}'",
-                                                   return_result=True)
-            return len(running) >= 1
-
-
-    # 1. 根据自己实际测试需要配置组装generator
-    generator = Pipeline([
-        gen.mix,
-        partial(gen.stagger, 1),
-        partial(gen.nemesis, None),
-        partial(gen.time_limit, 10)
-    ])([read, write, cas])
-    test(generator=generator, database=etcd_database, operation=operation)
+    # def operation(database_connection, history):
+    #     function_name = history["f"]
+    #     try:
+    #         if function_name == "write":
+    #             database_connection.put("foo", history["value"])
+    #             return {
+    #                 "type": "ok",
+    #                 "f": "write",
+    #                 "value": history["value"]
+    #             }
+    #         elif function_name == "read":
+    #             read_result = database_connection.get("foo")
+    #             return {
+    #                 "type": "ok",
+    #                 "f": "read",
+    #                 "value": int(read_result[0]) if read_result[0] else None
+    #             }
+    #         elif function_name == "cas":
+    #             cas_result = database_connection.replace("foo", history["value"][0], history["value"][1])
+    #             return {
+    #                 "type": "ok" if cas_result else "fail",
+    #                 "f": "cas",
+    #                 "value": history["value"]
+    #             }
+    #     except Exception as e:
+    #         logging.error(traceback.format_exc())
+    #         logging.error(repr(e))
+    #         return {
+    #             "type": "info",
+    #             "f": function_name,
+    #             "value": None
+    #         }
+    #
+    #
+    # def write():
+    #     return {
+    #         "type": "invoke",
+    #         "f": "write",
+    #         "value": str(random.randint(1, 5))
+    #     }
+    #
+    #
+    # def read():
+    #     return {
+    #         "type": "invoke",
+    #         "f": "read",
+    #         "value": None
+    #     }
+    #
+    #
+    # def cas():
+    #     return {
+    #         "type": "invoke",
+    #         "f": "cas",
+    #         "value": [str(random.randint(1, 5)), str(random.randint(1, 5))]
+    #     }
+    #
+    #
+    # class etcd_database(database_op):
+    #     def __init__(self, ssh_client, hostname, config):
+    #         super().__init__(ssh_client, hostname, config)
+    #         self.port = config["port"]
+    #         self.initial_cluster = config["initial_cluster"]
+    #         self.database_connection = None
+    #
+    #     def setup(self):
+    #         root_path = self.ssh_client.pwd() + "/tmp/"
+    #         self.ssh_client.wget(url="https://storage.googleapis.com/etcd/v3.1.5/etcd-v3.1.5-linux-amd64.tar.gz")
+    #         self.ssh_client.mkdir(root=root_path, filename="etcd")
+    #         self.ssh_client.unzip(file_name="{}etcd-v3.1.5-linux-amd64.tar.gz".format(root_path),
+    #                               opts={
+    #                                   "-C": root_path + "/etcd",
+    #                                   "--strip-components": 1
+    #                               })
+    #         self.ssh_client.touch(root=root_path, filename="etcd.log")
+    #         self.ssh_client.exec_sudo_command(command="{}etcd/etcd".format(root_path),
+    #                                           opts={
+    #                                               "--log-output": "stdout",
+    #                                               "--name": self.hostname,
+    #                                               "--listen-peer-urls": "http://0.0.0.0:2380",
+    #                                               "--listen-client-urls": "http://0.0.0.0:2379",
+    #                                               "--advertise-client-urls": "http://{0}:2379".format(self.hostname),
+    #                                               "--initial-cluster-state": "new",
+    #                                               "--initial-advertise-peer-urls": "http://{0}:2380".format(
+    #                                                   self.hostname),
+    #                                               "--initial-cluster": self.initial_cluster,
+    #                                               "1>{}etcd.log".format(root_path): ""
+    #                                           })
+    #
+    #     def shutdown(self):
+    #         root_path = self.ssh_client.pwd() + "/*"
+    #         self.ssh_client.kill_by_process("etcd")
+    #         self.ssh_client.exec_sudo_command("rm -rf {}".format(root_path))
+    #
+    #     def connect_database(self):
+    #         self.database_connection = etcd3.client(host=self.hostname, port=self.port)
+    #
+    #     def disconnect_database(self):
+    #         if self.database_connection:
+    #             self.database_connection.close()
+    #             self.database_connection = None
+    #         else:
+    #             pass
+    #
+    #     def is_running(self):
+    #         running = self.ssh_client.exec_command("ps -ef|grep etcd|grep -v grep|grep -v wget|awk '{print $2}'",
+    #                                                return_result=True)
+    #         return len(running) >= 1
+    #
+    #
+    # # 1. 根据自己实际测试需要配置组装generator
+    # generator = Pipeline([
+    #     gen.mix,
+    #     partial(gen.stagger, 1),
+    #     partial(gen.nemesis, None),
+    #     partial(gen.time_limit, 10)
+    # ])([read, write, cas])
+    test(generator=None, database=database_op, operation=None)
